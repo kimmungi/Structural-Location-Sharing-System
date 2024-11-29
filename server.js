bfe4e438259b41e7b10f381608982b5ecc0c6976
김문기 <rlaansrl1@naver.com>
2024-11-30 01:09:15 +0900

    Restore original files

--- server.js
+++ server.js
@@ -8,46 +8,45 @@ const app = express();
 const server = http.createServer(app);
 
 // CORS 설정
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // Socket.IO 설정
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
+    allowedHeaders: ["*"],
+    credentials: true,
   },
-  transports: ["polling"],
-  path: "/socket.io/",
+  pingTimeout: 60000,
+  transports: ["websocket", "polling"],
 });
 
 // PeerJS 서버 설정
 const peerServer = ExpressPeerServer(server, {
   debug: true,
-  path: "/peerjs",
+  path: "/",
   allow_discovery: true,
-  proxied: true,
-  ssl: {},
 });
 
 app.use("/peerjs", peerServer);
 app.use(express.static("public"));
 
-// 소켓 연결
+// 소켓 연결 처리
 io.on("connection", (socket) => {
   console.log("사용자 연결됨");
 
-  socket.on("disconnect", () => {
-    console.log("사용자 연결 해제");
-  });
-
   socket.on("locationUpdate", (location) => {
     socket.broadcast.emit("rescuerLocation", location);
+  });
+
+  socket.on("chatMessage", (message) => {
+    io.emit("chatMessage", message);
+  });
+
+  socket.on("joinRescueRoom", (peerId) => {
+    socket.join("rescueRoom");
+    socket.to("rescueRoom").emit("newRescuer", peerId);
   });
 });
 
